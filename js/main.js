2545b67f3373f5fd64fdabc97a1427e54faf41d2
Raven408RG <trek606@mail.ru>
2022-11-19 12:19:34 +0300

    Adaptive

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
 (function () {
-    const header = document.querySelector('.header');
+    const header = document.querySelector(".header");
     window.onscroll = () => {
         if (window.pageYOffset > 50) {
-            header.classList.add('header_active');
+            header.classList.add("header_active");
         } else {
-            header.classList.remove('header_active');
+            header.classList.remove("header_active");
         }
     };
-}());
-
+})();
 
 // Burger handler
 (function () {
-    const BurgerIteam = document.querySelector ('.burger');
-    const menu = document.querySelector ('.header__nav');
-    menuCloseItem = document.querySelector ('.header__nav-close');
-    BurgerIteam.addEventListener ('click', () => {
-        menu.classList.add ('header__nav_active');
-    })
-    menuCloseItem.addEventListener ('click', () =>{
-        menu.classList.remove ('header__nav_active');
+    const BurgerIteam = document.querySelector(".burger");
+    const menu = document.querySelector(".header__nav");
+    menuCloseItem = document.querySelector(".header__nav-close");
+    BurgerIteam.addEventListener("click", () => {
+        menu.classList.add("header__nav_active");
     });
-}());
+    menuCloseItem.addEventListener("click", () => {
+        menu.classList.remove("header__nav_active");
+    });
+})();
