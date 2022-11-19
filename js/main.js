935b7afc4342a8c0028f66af2f3e58895641b0b5
Raven408RG <trek606@mail.ru>
2022-11-19 20:59:29 +0300

    footer

--- js/main.js
+++ js/main.js
@@ -8,3 +8,17 @@
         }
     };
 }());
+
+
+// Burger handler
+(function () {
+    const BurgerIteam = document.querySelector ('.burger');
+    const menu = document.querySelector ('.header__nav');
+    menuCloseItem = document.querySelector ('.header__nav-close');
+    BurgerIteam.addEventListener ('click', () => {
+        menu.classList.add ('header__nav_active');
+    })
+    menuCloseItem.addEventListener ('click', () =>{
+        menu.classList.remove ('header__nav_active');
+    });
+}());
